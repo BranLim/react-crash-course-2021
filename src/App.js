373f33e8db01441503a61fcb78fdc50d31a4c877
Brandon Lim <limws.brandon@gmail.com>
2021-06-22 21:42:26 +0800

    Add message for no task. Fixed wrong CSS

--- src/App.js
+++ src/App.js
@@ -31,9 +31,14 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {
+        tasks.length > 0
+          ? (<Tasks tasks={tasks} onDelete={deleteTask} />)
+          : ('No Tasks To Show')
+      }
+
     </div>
   );
 }
