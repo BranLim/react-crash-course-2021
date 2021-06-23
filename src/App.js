65d2f704e3a4fda28ae7dca5f6f7fc2aad0d8321
Brandon Lim <limws.brandon@gmail.com>
2021-06-23 15:07:51 +0800

    Implement API call to json-server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
@@ -6,27 +6,16 @@ import AddTasks from './components/AddTasks'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2.30pm',
-      reminder: true
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1.30pm',
-      reminder: true
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2.30pm',
-      reminder: false,
-    }
+  const [tasks, setTasks] = useState([]);
 
-  ]);
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json();
+      
+    }
+    fetchTasks()
+  })
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1;
